1bd4563d14e0a1b9d042396c2c75eaac4f9ea205
JalalJahangir <jalaljahangir4@gmail.com>
2024-06-20 00:33:34 +0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = [ "Web Developer", "ReactJS Developer", "NextJS Developer" ];
-  const period = 2000;
+  const period = 1000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
